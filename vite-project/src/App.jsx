23a225595ea982e3a03d5b77c5738a0b020af61d
Siddhant <mahatosiddhant69@gmail.com>
2023-08-29 22:56:39 +0530

    companies

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Companies from "./components/Companies/Companies";
 function App() {
   return (
     <div>
       <Banner />
       <Navbar />
       <Hero />
+      <Companies />
     </div>
   );
 }
--- vite-project/src/components/Companies/Companies.jsx
+++ vite-project/src/components/Companies/Companies.jsx
@@ -1,7 +1,29 @@
+import "./Companies.css";
+import com1 from "../../assets/asset 3.png";
+import com2 from "../../assets/asset 4.png";
+import com3 from "../../assets/asset 5.png";
+import com4 from "../../assets/asset 6.png";
+import com5 from "../../assets/asset 7.png";
+import com6 from "../../assets/asset 8.svg";
+import com7 from "../../assets/asset 9.png";
 const Companies = () => {
   return (
     <section className="companies-section">
-      <div className="container"></div>
+      <div className="container company flex">
+        <div className="small-bold-text">
+          The world’s best companies rely on UsabilityHub to make better design
+          decisions.
+        </div>
+        <div className="company-img flex">
+          <img src={com1} className="one" />
+          <img src={com2} className="2" />
+          <img src={com3} className="third" />
+          <img src={com4} className="fourth" />
+          <img src={com5} className="fiveth" />
+          <img src={com6} className="6" />
+          <img src={com7} className="7" />
+        </div>
+      </div>
     </section>
   );
 };
