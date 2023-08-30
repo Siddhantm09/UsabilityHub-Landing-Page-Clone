533ee831ab3e90c628c7dbe20927c65aabc0caa9
Siddhant <mahatosiddhant69@gmail.com>
2023-08-30 19:41:17 +0530

    Feature-2

--- vite-project/src/utils/featureNames.js
+++ vite-project/src/utils/featureNames.js
@@ -1,32 +1,38 @@
+import img1 from '../assets/asset11.svg'
+import img2 from '../assets/asset 13.svg'
+import img3 from '../assets/asset 14.svg'
+import img4 from '../assets/asset 15.svg'
+import img5 from '../assets/asset 16.svg'
+import img7 from '../assets/asset 17.svg'
 
 const array = [
     {
-        image: '../assets/asset11.svg',
+        image: img1,
         name: "Card sorting",
         info: "Discover how people group and label information.",
     },
     {
-        image: "../../assets/asset 13.svg",
+        image: img2,
         name: "Prototype tests",
         info: "Discover how people navigate your Figma prototypes.",
     },
     {
-        image: "../assets/asset 14.svg",
+        image: img3,
         name: "First click tests",
         info: "Test interaction with first click and navigation tests.",
     },
     {
-        image: "../assets/asset 15.svg",
+        image: img4,
         name: "Surveys",
         info: "Get feedback on images, videos or audio files.",
     },
     {
-        image: "../assets/asset13.svg",
+        image: img5,
         name: "Preference tests",
         info: "Find out which designs users prefer and why.",
     },
     {
-        image: "../assets/asset13.svg",
+        image: img7,
         name: "Five second tests",
         info: "Test comprehensibility by measuring first impressions.",
     },
