556deaac59494d4883d1a076e8c52523f62679d0
Siddhant <mahatosiddhant69@gmail.com>
2023-09-17 00:59:08 +0530

    responsive

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import logo from "../../assets/asset 1.png";
 import logo1 from "../../assets/asset 33.svg";
 import "./Navbar.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
     <section>
@@ -45,6 +46,42 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+      </div>
+      {/* Responsive Code */}
+      <div className="burger-menu flex">
+        <FontAwesomeIcon icon={faBars} />
+        {/* <ul className="flex burger-ul">
+          <li>
+            <a href="#" className="hover-link">
+              Products
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-link">
+              Customers
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-link">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-link">
+              Resources
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-link secondary-btn">
+              Sign In
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover-link primary-btn">
+              Try for free
+            </a>
+          </li>
+        </ul> */}
       </div>
     </section>
   );
